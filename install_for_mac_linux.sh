6a2090b0ee46d6f572214475bb270410f44c299a
Pierre Poulain <pierre.poulain@cupnet.net>
2019-12-23 18:48:04 +0100

    Add name to containers

--- install_for_mac_linux.sh
+++ install_for_mac_linux.sh
@@ -5,7 +5,7 @@ script_name="autoclassweb-app.sh"
 docker pull pierrepo/autoclassweb:latest
 
 echo "#! /usr/bin/env bash" > ${script_name}
-echo "docker stop pierrepo/autoclassweb" >> ${script_name}
-echo "docker run --rm -d -p 5000:5000 -v $(pwd)/config:/app/config -v $(pwd)/logs:/app/logs -v $(pwd)/results:/app/results pierrepo/autoclassweb " >> ${script_name}
+echo "docker stop autoclassweb" >> ${script_name}
+echo "docker run --rm -d --name autoclassweb -p 5000:5000 -v $(pwd)/config:/app/config -v $(pwd)/logs:/app/logs -v $(pwd)/results:/app/results pierrepo/autoclassweb " >> ${script_name}
 
 chmod +x ${script_name}
