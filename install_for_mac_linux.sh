96bd7759acbdab673750f8b0fc7bdc0eca09f8b4
Pierre Poulain <pierre.poulain@cupnet.net>
2021-04-15 09:58:16 +0200

    Update Docker repo
    pierrepo -> biocontainers

--- install_for_mac_linux.sh
+++ install_for_mac_linux.sh
@@ -2,10 +2,15 @@
 
 script_name="autoclassweb-app.sh"
 
-docker pull pierrepo/autoclassweb:latest
+docker pull biocontainers/autoclassweb:v1.2.0_cv1
 
 echo "#! /usr/bin/env bash" > ${script_name}
 echo "docker stop autoclassweb" >> ${script_name}
-echo "docker run --rm -d --name autoclassweb -p 5000:5000 -v $(pwd)/config:/app/config -v $(pwd)/logs:/app/logs -v $(pwd)/results:/app/results pierrepo/autoclassweb " >> ${script_name}
+echo "docker run --rm -d --name autoclassweb -p 5000:5000 \\" >> ${script_name}
+echo "       -v $(pwd)/config:/app/config \\" >> ${script_name}
+echo "       -v $(pwd)/logs:/app/logs \\" >> ${script_name}
+echo "       -v $(pwd)/results:/app/results \\" >> ${script_name}
+echo "       biocontainers/autoclassweb:v1.2.0_cv1 \\" >> ${script_name}
+echo "       gunicorn --config /app/gunicorn.py flaskapp:app" >> ${script_name}
 
 chmod +x ${script_name}
