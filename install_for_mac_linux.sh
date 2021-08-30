35c5a19846244910e91b247a020c9d2cc7eb9d27
Pierre Poulain <pierre.poulain@cupnet.net>
2021-08-30 12:35:32 +0200

    Update to AutoClassWeb 2.2.1

--- install_for_mac_linux.sh
+++ install_for_mac_linux.sh
@@ -2,7 +2,7 @@
 
 script_name="autoclassweb-app.sh"
 
-docker pull biocontainers/autoclassweb:v2.1.1_cv1
+docker pull biocontainers/autoclassweb:v2.2.1_cv1
 
 echo "#! /usr/bin/env bash" > ${script_name}
 echo "docker stop autoclassweb" >> ${script_name}
@@ -10,7 +10,7 @@ echo "docker run --rm -d --name autoclassweb -p 5000:5000 \\" >> ${script_name}
 echo "       -v $(pwd)/config:/app/config \\" >> ${script_name}
 echo "       -v $(pwd)/logs:/app/logs \\" >> ${script_name}
 echo "       -v $(pwd)/results:/app/results \\" >> ${script_name}
-echo "       biocontainers/autoclassweb:v2.1.1_cv1 \\" >> ${script_name}
+echo "       biocontainers/autoclassweb:v2.2.1_cv1 \\" >> ${script_name}
 echo "       gunicorn --config /app/gunicorn.py flaskapp:app" >> ${script_name}
 
 chmod +x ${script_name}
